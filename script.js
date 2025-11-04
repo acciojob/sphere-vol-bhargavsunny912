d2ab82385b207cb1624abccc531893bcab183a78
Bhargav Penta <150350442+bhargavsunny912@users.noreply.github.com>
2025-11-04 13:37:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+	let rad=document.forms[0].radius;
+	let vol=document.forms[0].volume;
+
+	if(rad.value>=0 && rad.value/1){
+		let volume=((4/3)*(3.14)*((rad.value.trim())**2));
+		vol.value=volume.toFixed(4);
+		rad.value="";
+	}
+	else{
+		vol.value="NaN";
+		rad.value="";
+	}
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
